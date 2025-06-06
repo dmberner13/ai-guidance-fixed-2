8da0dd8ba12f772e16b3f40c161b82cf2b231125
dmberner13 <david.berner@swwc.org>
2025-06-06 15:13:36 -0500

    Create page.tsx

---
+++ AI Guidance Project/app/test/page.tsx
@@ -0,0 +1,9 @@
+export default function TestPage() {
+  return (
+    <div style={{ padding: "40px", textAlign: "center" }}>
+      <h1 style={{ fontSize: "24px", marginBottom: "20px" }}>Test Page Works!</h1>
+      <p>If you can see this, Next.js routing is working!</p>
+      <p>The problem is with missing components on the home page.</p>
+    </div>
+  )
+}
